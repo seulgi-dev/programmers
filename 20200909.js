ea9ddd0782b6686df096801b6dc8c3c1d29d580b
sg.lee.dev <sg.lee@hanqea.com>
2020-09-09 13:53:24 +0900

    20200909

---
+++ 20200909.js
@@ -0,0 +1,26 @@
+/*
+[문제 설명]
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+[제한사항]
+s는 길이가 1 이상, 100이하인 스트링입니다.
+
+[입출력 예]
+s	    return
+abcde	c
+*/
+function solution(s) {
+    var answer = '';
+    var len = s.length;
+    
+    // 짝수
+    if( len % 2 == 0 ){
+       answer = s.substring( (len/2)-1, (len/2)+1 );
+    // 홀수
+    }else{
+       answer = s.substring( (len/2), (len/2)+1 );
+    }
+    
+    return answer;
+}
