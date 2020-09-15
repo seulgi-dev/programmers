35a4f95114df39de087c9eef537c5ebe85d3fa60
이슬기 <62168341+seulgi-dev@users.noreply.github.com>
2020-09-15 15:21:43 +0900

    Create 서울에서 김서방 찾기.js

---
+++ 서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+/**
+ * 서울에서 김서방 찾기.js
+ * @url https://programmers.co.kr/learn/courses/30/lessons/12919?language=javascript
+ * @date 2020-09-15(화)
+ * @comment 
+ */
+
+function solution(seoul) {
+   return ("김서방은 " + seoul.indexOf("Kim") + "에 있다");
+}
