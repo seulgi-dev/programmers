06329ba56f322bacdabb7c461761a6264007b329
이슬기 <62168341+seulgi-dev@users.noreply.github.com>
2020-09-16 14:14:25 +0900

    Create 문자열을 정수로 바꾸기.js

---
+++ 문자열을 정수로 바꾸기.js
@@ -0,0 +1,9 @@
+/**
+ * 문자열을 정수로 바꾸기.js
+ * @url https://programmers.co.kr/learn/courses/30/lessons/12925?language=javascript
+ * @date 2020-09-16(수)
+ * @comment 
+ */
+function solution(s) {
+    return parseInt(s);
+}
