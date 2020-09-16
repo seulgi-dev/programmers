a21c6577677e4b5aebf75aa7d5ea637ee063df4f
이슬기 <62168341+seulgi-dev@users.noreply.github.com>
2020-09-16 14:17:31 +0900

    Create 짝수와 홀수.js

---
+++ 짝수와 홀수.js
@@ -0,0 +1,9 @@
+/**
+ * 짝수와 홀수.js
+ * @url https://programmers.co.kr/learn/courses/30/lessons/12937
+ * @date 2020-09-16(수)
+ * @comment 
+ */
+ function solution(num) {
+    return (num%2 == 0) ? "Even" : "Odd";
+}
