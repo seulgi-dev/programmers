a48588b682216979fae1abcee74170c6e1abe508
이슬기 <62168341+seulgi-dev@users.noreply.github.com>
2020-09-16 14:25:08 +0900

    Create 평균 구하기.js

---
+++ 평균 구하기.js
@@ -0,0 +1,9 @@
+/**
+ * 평균 구하기
+ * @url https://programmers.co.kr/learn/courses/30/lessons/12944
+ * @date 2020-09-16(수)
+ * @comment 
+ */
+function solution(arr) {
+    return arr.reduce((a, b) => a + b) / arr.length;
+}
