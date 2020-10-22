3f8ca23df56b31b4248cf69147c5eb43c830b236
이슬기 <62168341+seulgi-dev@users.noreply.github.com>
2020-10-22 19:23:02 +0900

    Create 크레인 인형뽑기 게임_미완.js

---
+++ 크레인 인형뽑기 게임_미완.js
@@ -0,0 +1,10 @@
+/**
+ * 크레인 인형뽑기 게임.js
+ * @url https://programmers.co.kr/learn/courses/30/lessons/64061
+ * @date 2020-10-22(목)
+ * @comment 문제 자체에 대한 이해가 안간다.
+ */
+function solution(board, moves) {
+    var answer = 0;
+    return answer;
+}
